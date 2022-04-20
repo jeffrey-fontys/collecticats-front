b5c5cd685acb1cb85a705392fd3054c67c422bc9
Jeffrey Derksen <jeffrey.derksen@student.fontys.nl>
2022-04-20 22:29:12 +0000

    Customization for CollectiCats

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-import logo from "../assets/logo.svg";
+import logo from "../assets/CollectiCats_logo_trans.png";
 
 const Hero = () => (
   <div className="text-center hero my-5">
-    <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
+    <img className="mb-3" src={logo} alt="CollectiCats logo" width="100%" />
     <h1 className="mb-4">React.js Sample Project</h1>
 
     <p className="lead">
